1010f88f022b5c6c2cbc7db1f73b2f34c34acf28
chandanpatra565 <76111375+chandanpatra565@users.noreply.github.com>
2022-02-25 12:26:32 +0530

    Add files via upload

---
+++ binarysearch recursion.java
@@ -0,0 +1,48 @@
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main (String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T>0)
+        {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for(int i=0;i<n;i++)
+            {
+                arr[i]=sc.nextInt();
+            }
+             
+            int key =sc.nextInt();
+            Solution g = new Solution();
+            System.out.println(g.binarysearch(arr,n,key));
+            T--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int binarysearch(int arr[], int n, int k){
+        return bi(arr,0,arr.length-1,k);
+    }
+    int bi(int[] arr,int low,int high,int k){
+        if(low>high) return -1;
+        int mid=low+(high-low)/2;
+        if(arr[mid]==k) return mid;
+        int res=0;
+        if(arr[mid]<k){
+            res=bi(arr,mid+1,high,k);
+        }else{
+            res=bi(arr,low,mid-1,k);
+        }
+        return res;
+    }
+}
